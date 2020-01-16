9bea1b8cb66dd15fafddb2e1ca857de38110d3d3
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 08:08:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,23 @@
 def map(source)
   return_array = []
   for i in source do
-    return_array.push(yield(i)
+    return_array.push(yield(i))
   end
   return_array
+end
+
+def reduce(source_array, starting_value = nil)
+ if(starting_value)
+   sum = starting_value
+   i = 0
+ else
+   sum = array[0]
+   i = 1
+  end
+  
+  while i < array.length do
+    sum = yield(sum, array[i])
+    i+=1
+  end
+  sum
 end
